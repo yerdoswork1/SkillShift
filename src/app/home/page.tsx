9dcdff4e5cadb001189d6187d4e4a1aef1199263
b1tzxrd <timkatey2004@gmail.com>
2025-06-09 14:30:41 +0500

    add favicon and title in home page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,10 +1,15 @@
 import HomePage from "@/components/pages/home-page"
+import type { Metadata } from "next";
 
+
+export const metadata: Metadata = {
+    title: "SkillShift - Главаная"
+};
 
 const Home = () => {
 
     return (
-        <HomePage/>
+        <HomePage />
     )
 }
 
